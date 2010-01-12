050015ff90074c09e4c9280fa0d93d595acfb594
Mario Limonciello <Mario_Limonciello@Dell.com>
2010-01-12 17:34:28 -0600

    Clean up the OEM config page to line up the widgets and add some nicer icons.

--- oem-config/dell-recovery.py
+++ oem-config/dell-recovery.py
@@ -51,9 +51,9 @@ class PageGtk(PluginUI):
                 self.dvd_media = builder.get_object('save_to_dvd')
                 self.none_media = builder.get_object('save_to_none')
                 if not dvd:
-                    builder.get_object('dvd_vbox').hide()
+                    builder.get_object('dvd_box').hide()
                 if not usb:
-                    builder.get_object('usb_vbox').hide()
+                    builder.get_object('usb_box').hide()
             except Exception, e:
                 self.debug('Could not create Dell Recovery page: %s', e)
                 self.plugin_widgets = None
