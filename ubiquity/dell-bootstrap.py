20cfa02178b96c6cae92b5fad1c1074649106b97
Mario Limonciello <Mario_Limonciello@Dell.com>
2010-02-23 15:49:17 -0600

    * dell-bootstrap:   - Don't eject the media for the user.  This appears to cause some problems with     current daily media in that portions of the squashfs needed for displaying     that dialog haven't yet been cached.

--- ubiquity/dell-bootstrap.py
+++ ubiquity/dell-bootstrap.py
@@ -211,9 +211,6 @@ class Page(Plugin):
         shutil.copy('/sbin/reboot', '/tmp')
         if self.kexec and os.path.exists('/cdrom/misc/kexec'):
             shutil.copy('/cdrom/misc/kexec', '/tmp')
-        eject = misc.execute_root('eject', '-p', '-m', '/cdrom')
-        if eject is False:
-            self.debug("Eject was: %d" % eject)
 
         #Set up a listen for udisks to let us know a usb device has left
         bus = dbus.SystemBus()
