d3e2c00a76f1fbe7a3b1211936e51010e9007b17
Mario Limonciello <Mario_Limonciello@Dell.com>
2010-04-21 23:11:43 -0500

    clean up the last test

--- tests/match-system-devices.py
+++ tests/match-system-devices.py
@@ -8,8 +8,7 @@ for line in pci_output.split('\n'):
     if line:
         vendor = line.split(':')[2].strip()
         product = line.split(':')[3].split()[0]
-        print "Trying to match PCI %s:%s."  % (vendor,product)
-        print match_system_device('pci','0x' + vendor, '0x' + product)
+        print "Trying to match PCI %s:%s (using python): %i."  % (vendor,product,match_system_device('pci','0x' + vendor, '0x' + product))
 
 usb_call = subprocess.Popen(['lsusb'], stdout = subprocess.PIPE)
 usb_output = usb_call.communicate()[0]
@@ -17,11 +16,4 @@ for line in pci_output.split('\n'):
     if line:
         vendor = line.split(':')[2].strip()
         product = line.split(':')[3].split()[0]
-        print "Trying to match USB %s:%s."  % (vendor,product)
-        print match_system_device('pci','0x' + vendor, '0x' + product)
-
-#print pci_devices
-#print match_system_device('usb','0x0fca','0x8004')
-#print match_system_device('usb',0x0fca,0x8004)
-#print match_system_device('pci','0x8086','0x2849')
-#print match_system_device('pci',0x8086,0x2849)
+        print "Trying to match USB %s:%s (using python): %i."  % (vendor,product,match_system_device('pci','0x' + vendor, '0x' + product))
