ada7a352d7cf87f0c5e4a70098c383f19934d261
Mario Limonciello <Mario_Limonciello@Dell.com>
2010-10-12 14:05:31 -0500

    Merge in the manufacturing site time zone logic.

--- late/chroot_scripts/95-set_UTC_TZ.sh
+++ late/chroot_scripts/95-set_UTC_TZ.sh
@@ -2,14 +2,13 @@
 #
 #       <95-set_UTC_TZ.sh>
 #
-#      This Script will run only once -AND-
-#       Will be removed by the SUCCESS script
-
+#      This Script will run only run if the manufacturing site was on /proc/cmdline
+#
 #       This script will update Local Time To UTC(GMT)
 #       The offsets in the dictionary below were lifted
 #       from the file tztable.xpe used by our sister group.
 #
-#       Copyright 2008 Dell Inc.
+#       Copyright 2008-2011 Dell Inc.
 #           Mario Limonciello <Mario_Limonciello@Dell.com>
 #           Hatim Amro <Hatim_Amro@Dell.com>
 #           Michael E Brown <Michael_E_Brown@Dell.com>
@@ -35,10 +34,64 @@
 # from 5:30 to 5
 # ==== Do we need to change this???
 
-if [ -f /cdrom/misc/tz_offset.py ] && [ -f /cdrom/misc/run-tz-fix ]; then
-    # Copy /cdrom/misc/tz_offset.py to /etc/init.d/tz_offset.py
-    cp -f /cdrom/misc/tz_offset.py /etc/init.d/tz_offset.py
-    cp -f /cdrom/misc/run-tz-fix /etc/init.d/run-tz-fix
+for arg in $(cat /proc/cmdline); do
+    if echo $arg | grep "MFGSITE=" 2>&1 >/dev/null; then
+        SITE=$(echo $arg | cut -d'=' -f2)
+    fi
+done
+
+if [ -n "$SITE" ]; then
+    cat > /etc/init.d/run-tz-fix <<EOF
+#!/bin/sh
+
+# Run tz_offset.py to fix the TZ adjust issue
+/usr/bin/python /etc/init.d/tz_offset.py
+
+# need to remove ThySelf here
+rm -rf /etc/init.d/run-tz-fix
+EOF
+
+    cat > /etc/init.d/tz_offset.py <<EOF
+#!/usr/bin/python
+
+# This Script will run only once on
+# the first boot.
+
+# This script will update Local Time To UTC(GMT)
+# The offsets in the dictionary below were lifted
+# from the file tztable.xpe used by our sister group.
+
+#DST is ignored since a large number of locales don't
+#use it.  See
+# http://en.wikipedia.org/wiki/Daylight_saving_time_around_the_world
+# for more information
+
+import os
+from datetime import datetime, timedelta
+
+Dict = {'amf':-5, 'apcc':+8, 'bcc':+3, 'ccc':+9, 'emf':+0, 'icc':+5, 'tcc':-6}
+
+# The #VALUE# is substituted at image download time
+# Dont edit the following line
+EOF
+    echo "MFGSITE = '$SITE'" >> /etc/init.d/tz_offset.py
+    cat > /etc/init.d/tz_offset.py <<EOF
+
+# Pick a MFGSITE OR set default value
+FACTORY_OFFSET = Dict['amf']
+if MFGSITE.lower() in Dict:
+        FACTORY_OFFSET = Dict[MFGSITE.lower()]
+
+offset = timedelta(hours=FACTORY_OFFSET)
+current_date = datetime.today()
+new_date = current_date - offset
+
+os.system('date --set="' + new_date.strftime('%d %b %Y %H:%M') + '"')
+os.system('hwclock --systohc')
+os.remove('/etc/init.d/tz_offset.py')
+os.remove('/etc/rc2.d/S02_force_utc')
+EOF
+
     chmod +x /etc/init.d/tz_offset.py
     chmod +x /etc/init.d/run-tz-fix
     # link it to /etc/rc2.d/S02_force_utc
