e0b7c6892a630238fa707707f0dc2f29af22ffa3
Mario Limonciello <Mario_Limonciello@Dell.com>
2011-10-06 14:04:12 -0500

    Don't disable os-prober on dual boot installs, only on factory and EFI installs.

--- late/chroot_scripts/02-grub.sh
+++ late/chroot_scripts/02-grub.sh
@@ -25,8 +25,8 @@
 
 . /usr/share/dell/scripts/fifuncs ""
 
-#make sure we have os-prober turned off one last time
-if ! grep "^GRUB_DISABLE_OS_PROBER" /etc/default/grub >/dev/null; then
+#If EFI install, turn off OS prober once more since we switched bootloader
+if [ -d /sys/firmware/efi ] && ! grep "^GRUB_DISABLE_OS_PROBER" /etc/default/grub >/dev/null; then
     echo "GRUB_DISABLE_OS_PROBER=true" >> /etc/default/grub
 fi
 
