1d6dd7a74b7bb64f8aecdc697f35f2c088651dfc
Mario Limonciello <mario_limonciello@dell.com>
2012-05-21 14:19:51 -0500

    ubiquity plugins: set shebang to python3 to match other ubiquity plugins.

--- ubiquity/dell-bootstrap.py
+++ ubiquity/dell-bootstrap.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python
+#!/usr/bin/python3
 # -*- coding: utf-8 -*-
 #
 # «dell-bootstrap» - Ubiquity plugin for Dell Factory Process
--- ubiquity/dell-recovery.py
+++ ubiquity/dell-recovery.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python
+#!/usr/bin/python3
 # -*- coding: utf-8 -*-
 #
 # «dell-recovery» - OEM Config plugin for Dell-Recovery Media
