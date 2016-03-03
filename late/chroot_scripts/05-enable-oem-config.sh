4a769ee951473f34e884b4f49d0f4e25a81f2a73
Mario Limonciello <Mario_Limonciello@Dell.com>
2016-03-03 10:50:27 -0600

    add missing 05-enable-oem-config.sh from earlier commit

---
+++ late/chroot_scripts/05-enable-oem-config.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+#
+#       <05-enable-oem-config>
+#
+#       Rerun update-grub for any changes from added packages or logical
+#       partition support that was added.
+#
+#       Copyright 2010 Dell Inc.
+#           Mario Limonciello <Mario_Limonciello@Dell.com>
+#
+#       This program is free software; you can redistribute it and/or modify
+#       it under the terms of the GNU General Public License as published by
+#       the Free Software Foundation; either version 2 of the License, or
+#       (at your option) any later version.
+#
+#       This program is distributed in the hope that it will be useful,
+#       but WITHOUT ANY WARRANTY; without even the implied warranty of
+#       MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#       GNU General Public License for more details.
+#
+#       You should have received a copy of the GNU General Public License
+#       along with this program; if not, write to the Free Software
+#       Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+#       MA 02110-1301, USA.
+
+. /usr/share/dell/scripts/fifuncs ""
+
+IFHALT "Enable oem-config systemd target"
+/bin/systemctl set-default oem-config.target
+IFHALT "Done with enable oem config"
