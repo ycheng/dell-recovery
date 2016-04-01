da921c5e4187f3ad67f41e39d3de8cd9b9d93674
Mario Limonciello <mario_limonciello@dell.com>
2016-04-01 10:36:50 -0500

    whitespace

--- secure_boot/MokSBStateSet.c
+++ secure_boot/MokSBStateSet.c
@@ -2,13 +2,13 @@
 
   Copyright (c) 2016, Canonical Limited. All rights reserved.
 
-  This program and the accompanying materials                          
-  are licensed and made available under the terms and conditions of the BSD License         
-  which accompanies this distribution.  The full text of the license may be found at        
-  http://opensource.org/licenses/bsd-license.php                                            
+  This program and the accompanying materials
+  are licensed and made available under the terms and conditions of the BSD License
+  which accompanies this distribution.  The full text of the license may be found at
+  http://opensource.org/licenses/bsd-license.php
 
-  THE PROGRAM IS DISTRIBUTED UNDER THE BSD LICENSE ON AN "AS IS" BASIS,                     
-  WITHOUT WARRANTIES OR REPRESENTATIONS OF ANY KIND, EITHER EXPRESS OR IMPLIED.             
+  THE PROGRAM IS DISTRIBUTED UNDER THE BSD LICENSE ON AN "AS IS" BASIS,
+  WITHOUT WARRANTIES OR REPRESENTATIONS OF ANY KIND, EITHER EXPRESS OR IMPLIED.
 
 **/
 
@@ -25,14 +25,14 @@ efi_main(EFI_HANDLE image, EFI_SYSTEM_TABLE *systab)
   EFI_GUID      VariableMoksbGuid = MOKSBSTATE_GUID;
   UINT8         Data=1;
 
-  VariableAttr = (EFI_VARIABLE_NON_VOLATILE|EFI_VARIABLE_BOOTSERVICE_ACCESS);	
+  VariableAttr = (EFI_VARIABLE_NON_VOLATILE|EFI_VARIABLE_BOOTSERVICE_ACCESS);
 
   systab->RuntimeServices->SetVariable (
-         L"MokSBState",     
-         &VariableMoksbGuid,   
-         VariableAttr,        
-         1, 
-         &Data          
+         L"MokSBState",
+         &VariableMoksbGuid,
+         VariableAttr,
+         1,
+         &Data
          );
 
   return EFI_SUCCESS;
