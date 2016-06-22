faaa704445c243b9b63a6fb5fa38a4824cc12ec8
Shih-Yuan Lee <fourdollars@gmail.com>
2016-06-22 07:27:34 -0500

    Only search for the prefix 'String 2:' and lowercase the BIOS ID. (#13)

--- late/chroot_scripts/03-ubuntu-drivers.sh
+++ late/chroot_scripts/03-ubuntu-drivers.sh
@@ -11,12 +11,11 @@ for i in `ubuntu-drivers list`; do
 done
 
 #install meta package based upon BIOS ID
-BIOS_ID=$(dmidecode -t 11 | sed '/String 2/!d; s,.*String 2: 1\[,,; s,\],,')
+BIOS_ID=$(dmidecode -t 11 | sed '/String 2:/!d; s,.*String 2: 1\[,,; s,\],,' | tr A-Z a-z)
 PACKAGE=dell-$BIOS_ID-meta
-    if ! dpkg-query -W $PACKAGE  >/dev/null 2>&1; then
-        apt-get install --yes $PACKAGE || true
-    fi
-
+if ! dpkg-query -W $PACKAGE >/dev/null 2>&1; then
+    apt-get install --yes $PACKAGE || true
+fi
 
 rm /etc/apt/apt.conf.d/99disable_authentication
 IFHALT "Done with ubuntu-drivers autoinstall"
