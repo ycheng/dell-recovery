e1e080535b7a32eb4a71238cc5a1284c0db2e035
Shih-Yuan Lee (FourDollars) <sylee@canonical.com>
2018-11-16 12:14:43 +0800

    Support meta by distribution.

--- late/chroot_scripts/03-ubuntu-drivers.sh
+++ late/chroot_scripts/03-ubuntu-drivers.sh
@@ -26,10 +26,12 @@ done
 
 #install meta package based upon BIOS ID
 BIOS_ID=$(dmidecode -t 11 | sed '/String 2:/!d; s,.*String 2: 1\[,,; s,\],,' | tr A-Z a-z)
-PACKAGE=dell-$BIOS_ID-meta
-if ! dpkg-query -W $PACKAGE >/dev/null 2>&1; then
-    apt-get install --yes $PACKAGE || true
-fi
+SERIES=$(lsb_release -cs)
+for pkg in dell-$BIOS_ID-meta dell-$BIOS_ID-$SERIES-meta; do
+    if ! dpkg-query -W $pkg >/dev/null 2>&1; then
+        apt-get install --yes $pkg || true
+    fi
+done
 
 rm /etc/apt/apt.conf.d/99disable_authentication
 IFHALT "Done with ubuntu-drivers autoinstall"
